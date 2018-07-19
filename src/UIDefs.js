758d86e41545dcbb8d5d5260d089db699a795298
Marvin Sanchez <iceman@Marvins-MacBook-Pro.local>
2018-07-19 17:18:45 +0800

    ..

--- src/UIDefs.js
+++ src/UIDefs.js
@@ -1,50 +1,49 @@
 export default {
-
     defaultContainerData: {
         flex: 1
     },
 
     tabs: {
-        general: { title: 'General', visible: false },
-        layout: { title: 'Layout', visible: false },
-        content: { title: 'Content', visible: false }
+        general: { title: "General", visible: false },
+        layout: { title: "Layout", visible: false },
+        content: { title: "Content", visible: false }
     },
 
     defaultProperties: [
         {
-            name: 'ID',
-            title: 'ID',
-            section: 'general',
-            component: 'form-control-text'
+            name: "ID",
+            title: "ID",
+            section: "general",
+            component: "form-control-text"
         },
         {
-            name: 'className',
-            title: 'Class',
-            section: 'general',
-            component: 'form-control-text'
+            name: "className",
+            title: "Class",
+            section: "general",
+            component: "form-control-text"
         },
         {
-            name: 'row',
-            title: 'Orientation',
-            section: 'layout',
-            values: ['Horizontal', 'Vertical'],
-            component: 'form-control-switch'
+            name: "row",
+            title: "Orientation",
+            section: "layout",
+            values: ["Horizontal", "Vertical"],
+            component: "form-control-switch"
         },
         {
-            name: 'flex',
-            title: 'Flex Weight',
-            placeholder: 'Flex weight 0 to 12',
-            section: 'layout',
+            name: "flex",
+            title: "Flex Weight",
+            placeholder: "Flex weight 0 to 12",
+            section: "layout",
             min: 0,
             max: 12,
-            component: 'form-control-number'
+            component: "form-control-number"
         },
         {
-            name: 'box',
-            title: 'Box Sizing',
-            placeholder: 'Flex weight 0 to 12',
-            section: 'layout',
-            component: 'form-control-box'
+            name: "box",
+            title: "Box Sizing",
+            placeholder: "Flex weight 0 to 12",
+            section: "layout",
+            component: "form-control-box"
         }
     ]
 };
--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
-import Vue from 'vue';
-import App from './App.vue';
-import Store from './stores';
-import Tree from './libs/tree.js';
+import Vue from "vue";
+import App from "./App.vue";
+import Store from "./stores";
+import Tree from "./libs/tree.js";
 
-import Buefy from 'buefy';
+import Buefy from "buefy";
 Vue.use(Buefy);
-Vue.component('editor-tinymce', TinymceVue.default);
+Vue.component("editor-tinymce", TinymceVue.default);
 
-import { codemirror } from 'vue-codemirror-lite';
-Vue.component('codemirror', codemirror);
-require('codemirror/mode/htmlmixed/htmlmixed');
-require('codemirror/mode/css/css');
+import { codemirror } from "vue-codemirror-lite";
+Vue.component("codemirror", codemirror);
+require("codemirror/mode/htmlmixed/htmlmixed");
+require("codemirror/mode/css/css");
 
 Vue.prototype.$tree = Tree;
 
@@ -19,16 +19,16 @@ Vue.prototype.$tree = Tree;
 //--------------
 Vue.prototype.$elements = {};
 Vue.prototype.$registerElement = component => {
-    Vue.prototype.$elements[component.name] = component;
-    Vue.component(component.name, component);
+	Vue.prototype.$elements[component.name] = component;
+	Vue.component(component.name, component);
 };
 
-import AddElement from './elements/AddElement';
-import RawHTML from './elements/RawHTML';
-import TextBlock from './elements/TextBlock';
-import Heading from './elements/Heading';
-import Image from './elements/Image';
-import Button from './elements/Button';
+import AddElement from "./elements/AddElement";
+import RawHTML from "./elements/RawHTML";
+import TextBlock from "./elements/TextBlock";
+import Heading from "./elements/Heading";
+import Image from "./elements/Image";
+import Button from "./elements/Button";
 
 Vue.prototype.$registerElement(AddElement);
 Vue.prototype.$registerElement(RawHTML);
@@ -38,7 +38,7 @@ Vue.prototype.$registerElement(Image);
 Vue.prototype.$registerElement(Button);
 
 new Vue({
-    el: '#app',
-    store: Store,
-    render: h => h(App)
+	el: "#app",
+	store: Store,
+	render: h => h(App)
 });
